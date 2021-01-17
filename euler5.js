c9d3f0467e0ed289ee99315c67312eb89768ace9
wazebase <50338875+wazebase@users.noreply.github.com>
2021-01-17 09:03:30 -0800

    add description

--- euler5.js
+++ euler5.js
@@ -1,3 +1,8 @@
+/* Project Euler: Problem 5: Smallest multiple
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to n?
+*/
 function smallestMult(n) {
  let anwser = 1 ;
   let numArr = [];
